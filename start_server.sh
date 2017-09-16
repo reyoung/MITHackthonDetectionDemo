ae10389c87b32516077fea1defcc4b8a95c8cd9e
Yu Yang <yuyang18@baidu.com>
2017-09-15 20:27:21 -0700

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://s3.us-east-2.amazonaws.com/models.paddlepaddle/SSD/param.tar
 wget https://s3.us-east-2.amazonaws.com/models.paddlepaddle/SSD/inference_topology.pkl
-nvidia-docker run --name detection_serv -d -v $PWD:/data -p 9000:80 -e WITH_GPU=1 paddlepaddle/book:serve
+nvidia-docker run --name detection_serv -d -v $PWD:/data -p 9000:80 -e WITH_GPU=1 paddlepaddle/book:serve-gpu
